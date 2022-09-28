2211b19451bd7a206e41c9690464526f65a1f566
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2022-09-27 20:25:42 -0700

    Hard code flasks and materials

--- scripts/craftable.js
+++ scripts/craftable.js
@@ -27,3 +27,30 @@ class Elixir extends Craftable {}
 class Potion extends Craftable {}
 
 class Transmutation extends Craftable {}
+
+const flasks = [
+  new Flask("Flask of Endless Rage", [
+    [Materials.lichbloom, 7],
+    [Materials.goldclover, 3],
+    [Materials.frostLotus, 1],
+    [Materials.enchantedVial, 1],
+  ]),
+  new Flask("Flask of Pure Mojo", [
+    [Materials.icethorn, 7],
+    [Materials.pygmyOil, 3],
+    [Materials.frostLotus, 1],
+    [Materials.enchantedVial, 1],
+  ]),
+  new Flask("Flask of Stoneblood", [
+    [Materials.lichbloom, 7],
+    [Materials.crystallizedLife, 3],
+    [Materials.frostLotus, 1],
+    [Materials.enchantedVial, 1],
+  ]),
+  new Flask("Flask of the Frost Wyrm", [
+    [Materials.icethorn, 7],
+    [Materials.lichbloom, 5],
+    [Materials.frostLotus, 1],
+    [Materials.enchantedVial, 1],
+  ]),
+];
--- scripts/material.js
+++ scripts/material.js
@@ -5,3 +5,13 @@ class Material {
     // ToDo: wowhead id
   }
 }
+
+const Materials = {
+  lichbloom: new Material("Lichbloom"),
+  goldclover: new Material("Goldclover"),
+  icethorn: new Material("Icethorn"),
+  frostLotus: new Material("Frost Lotus"),
+  pygmyOil: new Material("Pygmy Oil"),
+  enchantedVial: new Material("Enchanted Vial"),
+  crystallizedLife: new Material("Crystallized Life"),
+};
