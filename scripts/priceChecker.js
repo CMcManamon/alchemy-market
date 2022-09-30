dc8248a992cc31565f60900b3f6a284268506a91
CMcManamon <66437184+CMcManamon@users.noreply.github.com>
2022-09-29 21:51:09 -0700

    Let PriceChecker.fetchPrice() produce test values

--- scripts/priceChecker.js
+++ scripts/priceChecker.js
@@ -1,3 +1,4 @@
+// toDo: make singleton
 class PriceChecker {
   items;
   constructor() {
@@ -6,12 +7,18 @@ class PriceChecker {
 
   scanAll() {
     // Retrieves/updates price data for all items
-    for (const item in [...Materials, ...flasks]) {
-      let itemPrice = fetchPrice(item);
-      if (itemPrice != null) this.items.set(item, itemPrice);
+    for (const item in Materials) {
+      let itemPrice = this.#fetchPrice(item);
+      this.items.set(item, itemPrice.getString());
     }
   }
   scan(item) {}
   getPrice(item) {}
-  #fetchPrice(item) {}
+  #fetchPrice(item) {
+    // ToDo: If returned results are valid
+    let gold = Math.floor(Math.random() * 10);
+    let silver = Math.ceil(Math.random() * 99);
+    let copper = Math.floor(Math.random() * 100);
+    return new Currency(gold, silver, copper);
+  }
 }
